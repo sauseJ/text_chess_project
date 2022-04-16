61ccdaea678db8020227f1d7ab86daf946a469fc
sauseJ <sashakir00@gmail.com>
2022-04-16 20:26:56 +0300

    Added logic for Rooks and the class itself

--- chess/figures/figures_functions.py
+++ chess/figures/figures_functions.py
@@ -52,3 +52,48 @@ def checking_diogonals(figure, board, up_number, down_number, up_letter, down_le
         figure.possible_moves.append(possible_move_dd)
         leap_dd +=1
         
+def checking_all_sides(figure, board, letter, number, up_number, down_number, up_letter, down_letter, all_squares, leap_u=0, leap_d=0, leap_l=0, leap_r=0):
+
+    possible_move_u = f'{letter} + {up_number}'
+    figure_u = find_figure_by_position(board.all_figures, possible_move_u)
+    possible_move_d = f'{letter} + {down_number}'
+    figure_d = find_figure_by_position(board.all_figures, possible_move_d)
+    possible_move_r = f'{up_letter} + {number}'
+    figure_l = find_figure_by_position(board.all_figures, possible_move_r)
+    possible_move_l = f'{down_letter} + {number}'
+    figure_r = find_figure_by_position(board.all_figures, possible_move_l)
+
+    if (possible_move_u in all_squares) and (possible_move_u not in board.taken_squares):
+        figure.possible_moves.append(possible_move_u)
+
+    if (possible_move_u in all_squares) and (possible_move_u in board.taken_squares) \
+    and (leap_u < 1) and (figure.color != figure_u.color):
+        figure.possible_moves.append(possible_move_u)
+        leap_u +=1
+
+
+    if (possible_move_d in all_squares) and (possible_move_d not in board.taken_squares):
+        figure.possible_moves.append(possible_move_d)
+
+    if (possible_move_d in all_squares) and (possible_move_d in board.taken_squares) \
+    and (leap_d < 1) and (figure.color != figure_d.color):
+        figure.possible_moves.append(possible_move_d)
+        leap_d +=1
+
+    
+    if (possible_move_l in all_squares) and (possible_move_l not in board.taken_squares):
+        figure.possible_moves.append(possible_move_l)
+
+    if (possible_move_l in all_squares) and (possible_move_l in board.taken_squares) \
+    and (leap_l < 1) and (figure.color != figure_l.color):
+        figure.possible_moves.append(possible_move_l)
+        leap_l +=1
+
+
+    if (possible_move_r in all_squares) and (possible_move_u not in board.taken_squares):
+        figure.possible_moves.append(possible_move_u)
+
+    if (possible_move_r in all_squares) and (possible_move_r in board.taken_squares) \
+    and (leap_r < 1) and (figure.color != figure_r.color):
+        figure.possible_moves.append(possible_move_r)
+        leap_r +=1
