75e94f3af01368d9e4da30b29e6c39e3227aa2af
sauseJ <sashakir00@gmail.com>
2022-04-17 16:10:52 +0300

    Added Queen class

---
+++ chess/figures/Queen.py
@@ -0,0 +1,35 @@
+from base import Figure
+from chess.figures.figures_functions import checking_all_sides, checking_diogonals
+
+class Queen(Figure):
+
+    def __init__():
+        pass
+
+    def check_possible_moves(self, board):
+        self.possible_moves = []
+        letter = self.current_position[0]
+        asc_letter = ord(letter)
+        number = self.current_position[1]
+
+        for x in range(1, 8, 1):
+            up_number = number + x
+            down_number = number - x
+            up_letter = asc_letter + x
+            down_letter = asc_letter - x
+            checking_all_sides(self, board, letter, number, up_number, down_number, up_letter, down_letter, board.all_squares)
+            checking_diogonals(self, board, up_number, down_number, up_letter, down_letter, board.all_squares)
+
+
+    def move(self, new_position):
+        if (self.is_alive == True) and new_position in self.possible_moves:
+            self.current_position = new_position
+        else:
+            print('This move is not possible. Please enter something sane')
+
+    def take(self, new_position, other_piece):
+        if (self.is_alive == True) and new_position in self.possible_moves:
+            self.current_position = new_position
+            other_piece.is_alive = False
+        else:
+            print('This move is not possible. Please enter something sane')
