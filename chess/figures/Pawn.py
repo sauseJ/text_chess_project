746b5cece56ab5308fe95faa26730a492162f72a
sauseJ <sashakir00@gmail.com>
2022-04-18 22:32:08 +0300

    Moved the logic to the figures_functions file to make things prettier

--- chess/figures/Pawn.py
+++ chess/figures/Pawn.py
@@ -1,3 +1,4 @@
+from chess.figures.figures_functions import possible_pawn_moves
 from figures_functions import find_figure_by_position
 from base import Figure
 
@@ -13,100 +14,7 @@ class Pawn(Figure):
         asc_letter = ord(letter)
         number = self.current_position[1]
 
-        if (self.color == 'w') and (self.first_move == True):
-            for i in range(2, 0, -1):
-                new_number = number + i
-                possible_move = f'{letter} + {new_number}'
-
-                if (possible_move) not in board.taken_squares:
-                    self.possible_moves.append(possible_move)
-
-
-        if (self.color == 'w') and (self.first_move == False):
-            new_number = number + 1
-            possible_move = f'{letter} + {new_number}'
-
-            if possible_move not in board.taken_squares:
-                self.possible_moves.append(possible_move)
-
-        
-        if self.color == 'w':
-            new_number = number + 1
-            for i in range(-1, 3, 2):
-                new_letter = chr(asc_letter + i)
-                possible_move = f'{new_letter} + {new_number}'
-                french_move = f'{new_letter} + {number}'
-
-                side_figure = (find_figure_by_position(board.all_figures, french_move))
-                if ('en_passant_possible' in dir(side_figure)):
-                    french_pawn = side_figure
-                other_figure = find_figure_by_position(board.all_figures, possible_move)
-
-                if ((possible_move in board.taken_squares) and (other_figure.color != 'w'))\
-                or\
-                ((possible_move not in board.taken_squares) and (french_pawn.color != 'w') and (french_pawn.en_passant_possible == True)):
-                    self.possible_moves.append(possible_move)
-
-
-        if (self.color == 'b') and (self.first_move == True):
-            for i in range(2, 0, -1):
-                new_number = number - i
-                possible_move = f'{letter} + {new_number}'
-
-                if (possible_move) not in board.taken_squares:
-                    self.possible_moves.append(possible_move)
-
-
-        if (self.color == 'b') and (self.first_move == False):
-            new_number = number - 1
-            possible_move = f'{letter} + {new_number}'
-
-            if possible_move not in board.taken_squares:
-                self.possible_moves.append(possible_move)
-
-        
-        if self.color == 'b':
-            new_number = number - 1
-            for i in range(-1, 3, 2):
-                new_letter = chr(asc_letter + i)
-                possible_move = f'{new_letter} + {new_number}'
-                french_move = f'{new_letter} + {number}'
-
-                side_figure = (find_figure_by_position(board.all_figures, french_move))
-                if ('en_passant_possible' in dir(side_figure)):
-                    french_pawn = side_figure
-                other_figure = find_figure_by_position(board.all_figures, possible_move)
-
-                if ((possible_move in board.taken_squares) and (other_figure.color != 'b'))\
-                or\
-                ((possible_move not in board.taken_squares) and (french_pawn.color != 'b') and (french_pawn.en_passant_possible == True)):
-                    self.possible_moves.append(possible_move)
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????
-# BITCH YOU BETTER NOT FORGET THAT THESE PIECES OF SHIT CAN TRANSFORM DO YOU HEAR ME??????????        
+        possible_pawn_moves(self, board, number, letter, asc_letter)
 
     def move(self, new_position):
         if (self.is_alive == True) and new_position in self.possible_moves:
