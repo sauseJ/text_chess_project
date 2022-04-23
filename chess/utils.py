519c12ed474c5db151ebef82dc0f2e262de537f3
sauseJ <sashakir00@gmail.com>
2022-04-23 13:05:34 +0300

    Added stalemate and mate check. Added figure check. Thinking about executing everything correctly further on

--- chess/utils.py
+++ chess/utils.py
@@ -1,4 +1,12 @@
 import random
+from chess.figures.figures_functions import find_figure_by_possible_move
+from chess.figures.Pawn import Pawn
+from chess.figures.Bishop import Bishop
+from chess.figures.Knight import Knight
+from chess.figures.Rook import Rook
+from chess.figures.Queen import Queen
+from chess.figures.King import King
+
 
 def register_user(user):
     user.name = input('Stage your name: ')
@@ -21,15 +29,42 @@ def random_side(user1, user2):
         user2.my_turn = True
         print(f'Looks like it\'s {user2.name}\'s turn to move now!')
 
-def figure_check(move, user):
+def figure_check(move, user, board):
     letter = move[0]
     if letter == 'B':
-        pass
+        bishop = find_figure_by_possible_move(board.all_figures, move, Bishop)
+        return bishop
+    if letter == 'N':
+        knight = find_figure_by_possible_move(board.all_figures, move, Knight)
+        return knight
+    if letter == 'R':
+        rook = find_figure_by_possible_move(board.all_figures, move, Rook)
+        return rook
+    if letter == 'Q':
+        queen = find_figure_by_possible_move(board.all_figures, move, Queen)
+        return queen
+    if letter == 'K':
+        king = find_figure_by_possible_move(board.all_figures, move, King)
+        return king
+    if letter in ['a','b','c','d','e','f','g','h']:
+        pawn = find_figure_by_possible_move(board.all_figures, move, Pawn)
+        
 
 
-def action(user):
+def action(user, board):
     move = input(f'{user.name}, what\'s your move then: ')
-    figure = figure_check(move, user)
+    figure = figure_check(move, user, board)
+    pass
+
+def check_for_stalemate(king):
+    if (king.possible_moves == False) and (king.in_check == False):
+        return True
+
+def check_for_mate(king):
+    if (king.possible_moves == False) and (king.in_check == True):
+        return True
+
+
 
 
 
