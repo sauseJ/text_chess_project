488e9ef3aeb79e12648165e635c63619cf23181e
sauseJ <sashakir00@gmail.com>
2022-04-23 23:11:05 +0300

    Fixed figure_check and finished action functions

--- chess/utils.py
+++ chess/utils.py
@@ -1,5 +1,5 @@
 import random
-from chess.figures.figures_functions import find_figure_by_position, find_figure_by_possible_move
+from chess.figures.figures_functions import find_figure_by_position, find_figure_by_possible_move, find_my_king, pawn_promotion
 from chess.figures.Pawn import Pawn
 from chess.figures.Bishop import Bishop
 from chess.figures.Knight import Knight
@@ -29,38 +29,47 @@ def random_side(user1, user2):
         user2.my_turn = True
         print(f'Looks like it\'s {user2.name}\'s turn to move now!')
 
-def figure_check(move, board):
+def figure_check(move, board, user):
     letter = move[0]
     if letter == 'B':
-        bishop = find_figure_by_possible_move(board.all_figures, move, Bishop)
+        bishop = find_figure_by_possible_move(board.all_figures, move, Bishop, user)
         return bishop
     if letter == 'N':
-        knight = find_figure_by_possible_move(board.all_figures, move, Knight)
+        knight = find_figure_by_possible_move(board.all_figures, move, Knight, user)
         return knight
     if letter == 'R':
-        rook = find_figure_by_possible_move(board.all_figures, move, Rook)
+        rook = find_figure_by_possible_move(board.all_figures, move, Rook, user)
         return rook
     if letter == 'Q':
-        queen = find_figure_by_possible_move(board.all_figures, move, Queen)
+        queen = find_figure_by_possible_move(board.all_figures, move, Queen, user)
         return queen
     if letter == 'K':
-        king = find_figure_by_possible_move(board.all_figures, move, King)
+        king = find_figure_by_possible_move(board.all_figures, move, King, user)
         return king
     if letter in ['a','b','c','d','e','f','g','h']:
-        pawn = find_figure_by_possible_move(board.all_figures, move, Pawn)
-        return pawn
+        pawn = find_figure_by_possible_move(board.all_figures, move, Pawn, user)
+        if '=' in move:
+            new_figure = pawn_promotion(pawn, board, move)
+            return new_figure
+        else:
+            return pawn
         
 
 
 def action(user, board):
     move = input(f'{user.name}, what\'s your move then: ')
-    figure = figure_check(move, board)
-
-    if 'x' in move:
-        opponent_figure = find_figure_by_position(board.all_figures, move[-2:])
-        figure.take(move[-2:], opponent_figure)
+    if move == ('O-O' or '0-0'):
+        find_my_king(user, board).short_castle()
+    elif move == ('O-O-O' or '0-0-0'):
+        find_my_king(user, board).long_castle()
     else:
-        figure.move(move[-2:])
+        figure = figure_check(move, board)
+
+        if 'x' in move:
+            opponent_figure = find_figure_by_position(board.all_figures, move[-2:])
+            figure.take(move[-2:], opponent_figure)
+        else:
+            figure.move(move[-2:])
 
 def check_for_stalemate(king):
     if (king.possible_moves == False) and (king.in_check == False):
